0b5019c4a75ba457105507e2ab59f64657b405cf
Br0cod3 <thespammingbott@gmail.com>
2025-11-21 13:48:25 +0100

    add: CountryRanking and CountryDetail components

---
+++ src/components/CountryDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CountryDetails() {
+
+}
+
+export default CountryDetails;
---
+++ src/components/CountryRanking.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function CountryRanking() {
+
+}
+
+export default CountryRanking;
