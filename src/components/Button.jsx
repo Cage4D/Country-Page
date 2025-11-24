38ec50081de52fc95149ea5c35071afbe21c0d47
Br0cod3 <thespammingbott@gmail.com>
2025-11-24 12:30:49 +0100

    passed parent state as props and implement local click state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 
-function Button({ children, region }) {
+function Button({ children, region, state, setState }) {
+    const [click, setClick] = React.useState(false)
+
     return <button 
-    className="toggle-button"
+    className={click === true ? `toggle-button toggle-button-click` : `toggle-button`}
     type="button"
-    data-region={region}>{ children }</button>
+    data-region={region}
+    onClick={event => {
+        setClick(!click)
+        setState([...state, event.target.getAttribute("data-region")])
+    }}>{ children }</button>
 }
 
 export default Button;
