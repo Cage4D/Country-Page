43e5be51f886a71b7b0f942d70f58d1e673657f1
Br0cod3 <thespammingbott@gmail.com>
2025-11-25 13:35:03 +0100

    implement: handleClick function to handle onClick events

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,21 @@
 import React from "react"
 
 function Button({ children, region, state, setState }) {
-    const [click, setClick] = React.useState(false)
+    const isActive = state.includes(region)
+
+    const handleClick = () => {
+        if (isActive) {
+        setState(state.filter(item => item!== region))
+    } else {
+        setState([...state, region])
+    }
+    }
 
     return <button 
-    className={click === true ? `toggle-button toggle-button-click` : `toggle-button`}
+    className={isActive === true ? `toggle-button toggle-button-click` : `toggle-button`}
     type="button"
     data-region={region}
-    onClick={event => {
-        setClick(!click)
-        setState([...state, event.target.getAttribute("data-region")])
-    }}>{ children }</button>
+    onClick={handleClick}>{ children }</button>
 }
 
 export default Button;
