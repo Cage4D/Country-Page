504576104d9351d8e38193c8991a2e921eab66b6
Br0cod3 <thespammingbott@gmail.com>
2025-11-25 13:32:25 +0100

    Initial commit

---
+++ src/components/API.jsx
@@ -0,0 +1,62 @@
+
+async function fetcher(endpoint) {
+  try {
+    const response = await fetch(endpoint);
+
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
+    const json = await response.json();
+
+    return json;
+  } catch (error) {
+    window.alert("Fetch error: ", error);
+    return [];
+  }
+}
+
+export async function RenderResults(
+  sortType,
+  memberOfUN = false,
+  independent = false,
+  regions = []
+) {
+  let countries = [];
+  try {
+    if (regions.length > 0) {
+      const regionUrls = regions.map(
+        (region) =>
+          `https://restcountries.com/v3.1/region/${region}?fields=name,population,cca2,area,unMember,independent,region`
+      );
+      const responses = await Promise.all(
+        regionUrls.map((url) => fetcher(url))
+      );
+      countries = responses.flat();
+    } else {
+      countries = await fetcher(
+        "https://restcountries.com/v3.1/all?fields=name,population,cca2,area,unMember,independent,region"
+      );
+    }
+
+    if (memberOfUN)
+      countries = countries.filter((country) => country.unMember === true);
+    if (independent)
+      countries = countries.filter((country) => country.independent === true);
+
+    let sorted;
+    if (sortType === "population") {
+      sorted = countries.sort((a, b) => b.population - a.population);
+    } else if (sortType === "alphabetical-order") {
+      sorted = countries.sort((a, b) => {
+        if (a.name.common < b.name.common) return -1;
+        if (a.name.common > b.name.common) return 1;
+        return 0;
+      });
+    } else {
+      sorted = countries.sort((a, b) => b.area - a.area);
+    }
+
+    return sorted;
+  } catch (error) {
+    window.alert("Error fetching countries: ", error);
+    return [];
+  }
+}
