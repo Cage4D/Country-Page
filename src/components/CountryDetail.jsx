4b585e9947cf169635ea66aea6f97bdf2e66b571
Br0cod3 <thespammingbott@gmail.com>
2025-11-27 18:05:19 +0100

    feat: create loading skeleton

--- src/components/CountryDetail.jsx
+++ src/components/CountryDetail.jsx
@@ -5,6 +5,7 @@ import { CountryContext } from "./CountryContext";
 function CountryDetails({ name }) {
   const { setIndividual } = React.useContext(CountryContext)
   const [info, setInfo] = React.useState(null);
+
   React.useEffect(() => {
     async function fetchDetails() {
       const CountryInfo = await RenderCountryInfo(name);
@@ -13,7 +14,71 @@ function CountryDetails({ name }) {
 
     fetchDetails();
   }, [name]);
-  if (!info) return <p>Loading....</p>
+
+  if (!info) return (
+    <>
+      <div className="detail-container">
+        <div className="country-head">
+          <div className="country-img-container">
+            <div className="loading-big"></div>
+          </div>
+          <div className="head">
+            <div className="loading-small"></div>
+          </div>
+          <div className="header-subsection">
+            <div className="header-subsection__tab">
+              <div className="loading-small"></div>
+            </div>
+            <div className="header-subsection__tab">
+              <div className="loading-small"></div>
+            </div>
+          </div>
+        </div>
+        <div className="country-body">
+          <p>Capital</p>
+          <div className="loading-small"></div>
+        </div>
+        <div className="country-body">
+          <p>Subregion</p>
+          <div className="loading-small"></div>
+        </div>
+        <div className="country-body">
+          <p>Language</p>
+          <div className="loading-small"></div>
+        </div>
+        <div className="country-body">
+          <p>Currencies</p>
+          <div className="loading-small"></div>
+        </div>
+        <div className="country-body">
+          <p>Continents</p>
+          <div className="loading-small"></div>
+        </div>
+        <div className="neighbouring-countries">
+          <p>Neighbouring Countries</p>
+          <div className="bordering-countries">
+            <div className="bordering-countries__group">
+              <div className="loading-big"></div>
+              <div className="loading-small"></div>
+            </div>
+            <div className="bordering-countries__group">
+              <div className="loading-big"></div>
+              <div className="loading-small"></div>
+            </div>
+            <div className="bordering-countries__group">
+              <div className="loading-big"></div>
+              <div className="loading-small"></div>
+            </div>
+          </div>
+          <div className="close-detail-component">
+            <button 
+            className="close-btn"
+            >Close</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
   return (
     <>
       <div className="detail-container">
